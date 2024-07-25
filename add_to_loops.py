f94f64349418cd5d46ee5e97b8ea0d16d41bfb35
matthew-heartful <matthew.heartful@gmail.com>
2024-07-24 19:02:13 -0700

    added a function to check if email already exists

--- add_to_loops.py
+++ add_to_loops.py
@@ -20,3 +20,23 @@ def add_to_loops(user):
         logging.info(f"Added {user['login']} to Loops")
     else:
         logging.error(f"Error adding {user['login']} to Loops: {response.status_code}")
+
+def check_user_existence_by_email(email):
+    headers = {
+        "Authorization": f"Bearer {LOOPS_API_KEY}",
+        "Content-Type": "application/json"
+    }
+    url = f"https://app.loops.so/api/v1/contacts/find?email={email}"
+    logging.info(f"Checking existence of user with email {email} at {url}")
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        contacts = response.json()
+        if contacts:
+            # logging.info(f"User with email {email} exists in Loops")
+            return True
+        else:
+            logging.info(f"User with email {email} does not exist in Loops")
+            return False
+    else:
+        logging.error(f"Error checking user existence: {response.status_code} - {response.text}")
+        return False
