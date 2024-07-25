fbd63e15bc66b9f3b27468d1db83bdc458e266a1
matthew-heartful <matthew.heartful@gmail.com>
2024-07-24 19:03:18 -0700

    added step to check if email already exists in loops

--- process_stargazer.py
+++ process_stargazer.py
@@ -2,7 +2,7 @@ import requests
 import json
 import logging
 from config import GITHUB_TOKEN
-from add_to_loops import add_to_loops
+from add_to_loops import add_to_loops, check_user_existence_by_email
 
 def process_stargazer(stargazer):
     headers = {"Authorization": f"token {GITHUB_TOKEN}"} if GITHUB_TOKEN else {}
@@ -17,24 +17,27 @@ def process_stargazer(stargazer):
         }
         logging.info(f"User info: {user_info}")
         if user_info["url"]:
-            logging.info("Loading existing stargazers data")
+            # logging.info("Loading existing stargazers data")
             with open('stargazers.json', 'r') as infile:
                 stargazers = json.load(infile)
             
-            logging.info("Updating user info in stargazers data")
+            # logging.info("Updating user info in stargazers data")
             for stargazer in stargazers:
                 if stargazer["url"] == user_info["url"]:
-                    if "loops" not in stargazer:
-                        stargazer.update(user_info)
-                        # Add or update the loops field
-                        if user_info["email"]:
+                    stargazer.update(user_info)
+                    # Add or update the loops field
+                    if user_info["email"]:
+                        if not check_user_existence_by_email(user_info["email"]):
                             add_to_loops(user_info)
                             stargazer["loops"] = "added"
                         else:
-                            stargazer["loops"] = "no valid email exist"
-                            logging.warning("User does not have a valid email, skipping loops addition")
+                            stargazer["loops"] = "already exists"
+                            logging.info(f"User with email {user_info['email']} already exists in Loops")
+                    else:
+                        stargazer["loops"] = "no valid email exist"
+                        logging.warning("User does not have a valid email, skipping loops addition")
             
-            logging.info("Saving updated stargazers data")
+            # logging.info("Saving updated stargazers data")
             with open('stargazers.json', 'w') as outfile:
                 json.dump(stargazers, outfile, indent=4)
     else:
