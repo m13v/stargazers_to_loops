753141302ce48b1b98200ce2a457ffb5aa7b91ef
matthew-heartful <matthew.heartful@gmail.com>
2024-09-02 17:54:12 -0700

    correct api link

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import dotenv
 
 dotenv.load_dotenv()
 
-GITHUB_API = "https://github.com/mediar-ai/screenpipe/stargazers"
+GITHUB_API = "https://api.github.com/repos/mediar-ai/screenpipe/stargazers"
 LOOPS_API = "https://app.loops.so/api/v1/contacts/create"
 GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
 LOOPS_API_KEY = os.environ.get("LOOPS_API_KEY")
