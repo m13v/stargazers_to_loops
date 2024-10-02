f6fd08c5617ac66c1b82e090fdc5c22975774966
matthew-heartful <matthew.heartful@gmail.com>
2024-10-01 21:46:03 -0700

    back to mediar

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ import dotenv
 
 dotenv.load_dotenv()
 
-GITHUB_API = "https://api.github.com/repos/mediar-ai/screenpipe/stargazers"
+GITHUB_REPO = "mediar-ai/screenpipe" # mediar-ai/screenpipe / m13v/whatsapp2llm
+GITHUB_API = f"https://api.github.com/repos/{GITHUB_REPO}/stargazers"
 LOOPS_API = "https://app.loops.so/api/v1/contacts/create"
 GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
 LOOPS_API_KEY = os.environ.get("LOOPS_API_KEY")
